74bebf2df406ca94848fc29d23cd4f7d70ffa514
jchaz22 <jason.chaskin@gmail.com>
2024-01-27 23:35:07 -0500

    hi mom

--- app/page.js
+++ app/page.js
@@ -5,21 +5,12 @@ export async function generateMetadata() {
     other: (
       <>
         <meta property="og:title" content="Frame" />
-        <meta
-          property="og:image"
-          content={`${process.env.BASE_URL}/question.jpg`}
-        />
+        <meta property="og:image" content="/question.jpg" />
         <meta property="fc:frame" content="vNext" />
-        <meta
-          property="fc:frame:image"
-          content={`${process.env.BASE_URL}/question.jpg`}
-        />
+        <meta property="fc:frame:image" content="/question.jpg" />
         <meta property="fc:frame:button:1" content="Yes" />
         <meta property="fc:frame:button:2" content="No" />
-        <meta
-          property="fc:frame:post_url"
-          content={`${process.env.BASE_URL}/api/post`}
-        />
+        <meta property="fc:frame:post_url" content="/api/post" />
       </>
     ),
   };
