7dc19fdf1f977dfd6f34c79077f92c90edcb3969
jchaz22 <jason.chaskin@gmail.com>
2024-01-28 00:55:58 -0500

    last try

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,7 @@
+import { NextResponse } from "next/server";
 import { BASE_URL, generateFarcasterFrame, validateMessage } from "../utils";
 
-export async function POST(req, res) {
+export async function POST(req) {
   const signedMessage = await req.json();
 
   // Validate the signed message if it exists
@@ -10,21 +11,27 @@ export async function POST(req, res) {
     );
 
     if (!isMessageValid) {
-      return res.status(400).json({ error: "Invalid message" });
+      return NextResponse.json({ error: "Invalid message" }, { status: 400 });
     }
   }
 
   const choice = signedMessage.untrustedData.buttonIndex;
-  let html = "";
+  let htmlContent = "";
 
   // Generate HTML based on the choice
   if (choice === 1) {
-    html = generateFarcasterFrame(`${BASE_URL}/happy.jpg`, choice);
+    htmlContent = generateFarcasterFrame(`${BASE_URL}/happy.jpg`, choice);
   } else {
-    html = generateFarcasterFrame(`${BASE_URL}/threat.jpg`, choice);
+    htmlContent = generateFarcasterFrame(`${BASE_URL}/threat.jpg`, choice);
   }
 
-  // Send the response
-  res.setHeader("Content-Type", "text/html");
-  return res.status(200).send(html);
+  // Create a new response with HTML content
+  const response = new NextResponse(htmlContent, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+
+  return response;
 }
