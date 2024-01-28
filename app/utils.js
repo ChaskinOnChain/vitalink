30136d3bf347b626f87c0260063d2d430c36c561
jchaz22 <jason.chaskin@gmail.com>
2024-01-28 01:22:56 -0500

    testing again 2

--- app/utils.js
+++ app/utils.js
@@ -3,7 +3,7 @@ import { getHubRpcClient, Message } from "@farcaster/hub-web";
 export const BASE_URL = process.env.BASE_URL;
 
 // generate an html page with the relevant opengraph tags
-export function generateFarcasterFrame(image, choice) {
+export function generateFarcasterFrame(image) {
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -11,12 +11,6 @@ export function generateFarcasterFrame(image, choice) {
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
       <meta property="fc:frame:post_url" content="${BASE_URL}/api/post" />
-
-      ${
-        choice === 2
-          ? '<meta property="fc:frame:button:1" content="Yes" />'
-          : ""
-      }
     </head>
     <body>
       
