cd331a1c9b6e46a584ae48b562f367b2123b2370
jchaz22 <jason.chaskin@gmail.com>
2024-01-27 23:45:03 -0500

    will this work

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import { BASE_URL } from "./utils";
+
 export async function generateMetadata() {
   return {
     title: "Eventcaster RSVP test",
@@ -5,12 +7,12 @@ export async function generateMetadata() {
     other: (
       <>
         <meta property="og:title" content="Frame" />
-        <meta property="og:image" content="/question.jpg" />
+        <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="/question.jpg" />
+        <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame:button:1" content="Yes" />
         <meta property="fc:frame:button:2" content="No" />
-        <meta property="fc:frame:post_url" content="/api/post" />
+        <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
       </>
     ),
   };
