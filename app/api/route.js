d26ed162000710dee0867af15cc4d26d5eb40b31
jchaz22 <jason.chaskin@gmail.com>
2024-01-28 13:04:51 -0500

    is this it

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { generateFarcasterFrame, validateMessage } from "../utils";
-
+// hi
 export async function POST(req) {
   console.log("POST request received");
 
