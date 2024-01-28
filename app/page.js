ef7fa3064825b22004491af0649429003660c78d
jchaz22 <jason.chaskin@gmail.com>
2024-01-28 00:13:03 -0500

    hb now

--- app/page.js
+++ app/page.js
@@ -1,3 +1,22 @@
+export async function generateMetadata() {
+  return {
+    title: "Soup test",
+    description: "Soup event",
+    openGraph: {
+      title: "Eventcaster RSVP test",
+      images: [`${process.env.BASE_URL}/question.jpg`],
+    },
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": `${process.env.BASE_URL}/question.jpg`,
+      "fc:frame:button:1": "Yes",
+      "fc:frame:button:2": "No",
+      "fc:frame:post_url": `${process.env.BASE_URL}/api`,
+    },
+    metadataBase: new URL(process.env.BASE_URL ?? ""),
+  };
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
