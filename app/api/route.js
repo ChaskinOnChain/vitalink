99ad856f0527b438a2478561416fd868daed98f2
jchaz22 <jason.chaskin@gmail.com>
2024-01-28 02:04:02 -0500

    test

--- app/api/route.js
+++ app/api/route.js
@@ -1,43 +1,60 @@
 import { NextResponse } from "next/server";
 import { generateFarcasterFrame, validateMessage } from "../utils";
-// ugh
+
 export async function POST(req) {
-  const signedMessage = await req.json();
+  console.log("POST request received");
 
-  // Validate the signed message if it exists
-  if (signedMessage.trustedData) {
-    const isMessageValid = await validateMessage(
-      signedMessage.trustedData.messageBytes
-    );
+  try {
+    const signedMessage = await req.json();
+    console.log("Signed Message:", signedMessage);
 
-    if (!isMessageValid) {
-      return NextResponse.json({ error: "Invalid message" }, { status: 400 });
+    // Validate the signed message if it exists
+    if (signedMessage.trustedData) {
+      console.log("Validating trusted data");
+      const isMessageValid = await validateMessage(
+        signedMessage.trustedData.messageBytes
+      );
+      console.log("Is message valid:", isMessageValid);
+
+      if (!isMessageValid) {
+        console.error("Invalid message");
+        return NextResponse.json({ error: "Invalid message" }, { status: 400 });
+      }
     }
-  }
 
-  const choice = signedMessage.untrustedData.buttonIndex;
-  let htmlContent = "";
+    const choice = signedMessage.untrustedData.buttonIndex;
+    console.log("Choice from untrusted data:", choice);
 
-  // Generate HTML based on the choice
-  if (choice === 1) {
-    htmlContent = generateFarcasterFrame(
-      `${process.env.BASE_URL}/happy.jpg`,
-      choice
+    let htmlContent = "";
+
+    // Generate HTML based on the choice
+    if (choice === 1) {
+      htmlContent = generateFarcasterFrame(
+        `${process.env.BASE_URL}/happy.jpg`,
+        choice
+      );
+    } else {
+      htmlContent = generateFarcasterFrame(
+        `${process.env.BASE_URL}/threat.jpg`,
+        choice
+      );
+    }
+    console.log("Generated HTML content:", htmlContent);
+
+    // Create a new response with HTML content
+    const response = new NextResponse(htmlContent, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Error in POST function:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
     );
-  } else {
-    htmlContent = generateFarcasterFrame(
-      `${process.env.BASE_URL}/threat.jpg`,
-      choice
-    );
   }
-
-  // Create a new response with HTML content
-  const response = new NextResponse(htmlContent, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
-
-  return response;
 }
